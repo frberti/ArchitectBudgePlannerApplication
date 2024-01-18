9bbeeefa8214741a039ea6a65840248e2018bde1
Francisco Berti <francisco.berti@zarpsystem.com.br>
2024-01-18 11:34:14 -0300

    Implementando tratamento de exceptions

---
+++ src/main/kotlin/br/com/architectbudgeplanner/exception/ExceptionHandler.kt
@@ -0,0 +1,4 @@
+package br.com.architectbudgeplanner.exception
+
+class ExceptionHandler {
+}
---
+++ src/main/kotlin/br/com/architectbudgeplanner/exception/message/ErrorMessage.kt
@@ -0,0 +1,7 @@
+package br.com.architectbudgeplanner.exception.message
+
+class ErrorMessages {
+    companion object {
+        const val 
+    }
+}
