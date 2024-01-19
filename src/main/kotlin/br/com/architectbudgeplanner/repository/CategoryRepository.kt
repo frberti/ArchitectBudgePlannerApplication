11104dd51fd5fedbfe4ebf2817684e9c7c5fe6bf
Francisco Berti <francisco.berti@zarpsystem.com.br>
2024-01-19 15:06:27 -0300

    Adição de dependências

---
+++ src/main/kotlin/br/com/architectbudgeplanner/repository/CategoryRepository.kt
@@ -0,0 +1,4 @@
+package br.com.architectbudgeplanner.repository
+
+class CategoryRepository {
+}
---
+++ src/main/kotlin/br/com/architectbudgeplanner/repository/ClassCategoryRepository.kt
@@ -0,0 +1,7 @@
+package br.com.architectbudgeplanner.repository
+
+import br.com.architectbudgeplanner.model.Category
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface CategoryRepository: JpaRepository<Category, Long> {
+}
