0e5f51eb82699bb10a328bd6db683c181b722db2
Francisco Berti <francisco.berti@zarpsystem.com.br>
2024-01-25 10:17:23 -0300

    Adicionando modulo de cache (em memória) do springboot, para o ambiente de desenvolvimento

--- src/main/kotlin/br/com/architectbudgeplanner/ArchitectBudgePlannerApplication.kt
+++ src/main/kotlin/br/com/architectbudgeplanner/ArchitectBudgePlannerApplication.kt
@@ -2,8 +2,10 @@ package br.com.architectbudgeplanner
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
+@EnableCaching
 class ArchitectBudgePlannerApplication
 
 fun main(args: Array<String>) {
